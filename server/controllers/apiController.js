47fc5602b1323b6be857e1140a51e6aa15b3d3f8
Olivia-code <y.ofelya@gmail.com>
2021-08-25 22:57:27 -0700

    Server is in the process

---
+++ server/controllers/apiController.js
@@ -0,0 +1,33 @@
+const apiController = {};
+// like  function
+ apiController.like = (req, res, next)=> {
+   const user_id = res.locals.user_id;
+   const art_id = res.locals.art_id;
+   
+
+ }
+
+
+
+
+// Dislike funcs 
+
+module.exports = apiController;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {};
--- server/server.js
+++ server/server.js
@@ -1,35 +1,27 @@
 const express = require('express');
 const path = require('path');
-// import cookieParser from 'cookie-parser';
+const cookieParser = require('cookie-parser');
+
 // import dotenv from 'dotenv';
-// const userRouter = require('./routes/user.js');
-// const imageRouter = require('./routes/api.js');
-//const apiRouter = require('./routes/user')
+const apiRouter = require('./routes/apiRouter.js');
+const loginRouter = require('./routes/loginRouter.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // statically serve everything in the build folder on the route '/build'
 app.use(express.static(path.resolve('../build')));
 // TODO: comment out when ready Landing Page 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../index.html'));
-});
+
 
 // Login Page 
 
-app.get('/login',(req, res) => {
-  return res.redirect('/login');
-});
-
-// LOG OUT ?
-// app.get('/logout', (req, res) => {
-//   return res.redirect('/')
-// })
+app.use('/login', loginRouter);
+app.use('/api', apiRouter);
 
 // this is the call to get all the files from the sql DB 
 app.get ('/home', (req, res) => {
@@ -38,22 +30,13 @@ app.get ('/home', (req, res) => {
   return res.status(200).json(res.locals);
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../index.html'));
+});
+
 /**
  * define route handlers
  */
-
-// app.use('/api/user', userRouter);
-app.use('/api/user', (req, res)=>{
-  console.log(req.body, 'recieved at /api/user');
-  return res.send(req.body);
-});
-// app.use('/api/images', imageRouter);
-app.use('/api/images', (req, res)=>{
-  console.log(req.body, 'recieved at /api/images');
-  return res.send(req.body);
-});
-//app.use('/api', apiRouter);
-
 
 /**
  * 404 handler
