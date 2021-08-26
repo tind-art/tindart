dc1746baf9b5f404b02ec8a2d020cd7d7de8f2cc
Vivian Wu <vivian.wu.here@gmail.com>
2021-08-25 23:04:34 -0700

    update with uri and models

---
+++ server/models/tindartModel.js
@@ -0,0 +1,32 @@
+const { Pool } = require('pg');
+const uri = require('../config/uri.json');
+
+/*
+
+DB Setup
+
+PG_URI is a private variable which is not checked into source control.
+
+To config the app with elephant db:
+1 . add a config folder at the project root
+2.  add a uri.json file in the folder
+3.  inside the uri.json file add {"PG_URI": "<YOUR_DB_URI>"}
+
+https://www.elephantsql.com/docs/index.html
+
+*/
+
+// Import elephant db connection string.
+
+const PG_URI = uri.get('PG_URI');
+
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executed query', text);
+    return pool.query(text, params, callback);
+  },
+};
